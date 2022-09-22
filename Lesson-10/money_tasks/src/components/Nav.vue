3fd1e0103638529865f5cd6891a7f1ac4ff512ac
thanhminh <you@example.com>
2022-09-22 17:18:46 +0700

    Add Lesson-10

---
+++ Lesson-10/money_tasks/src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="nav">
+        <router-link to="/">Overview</router-link>
+        <router-link to="/transactions">Transactions</router-link>
+        <router-link to="/budgets">Budgets</router-link>
+        <router-link to="/reports">Reports</router-link>
+        <router-link to="/settings">Settings</router-link>
+    </nav>
+</template>
---
+++ Lesson-10/money_tasks/src/pages/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Homepage is here</h1>
+    <p> {{ firstname }} </p>
+    <p>{{car}}</p>
+    <button @click="onchangeSomething">Click me</button>
+</template>
+
+<script> 
+import {ref} from "vue";
+export default {
+    setup () {
+        const firstName = ref ("Sky Albert")
+        const sencondName = ref ({
+            name: "Quan",
+            something: "Minh"
+        })
+        const car = reactive({
+            price: 100000,
+            name: "Mec"
+        })
+
+        function onChangeSomething() {
+            console.log("running heree...");
+            console.log(sencondName);
+            car.price = 200000;
+            sencondName.value.name = "Hihi"
+            firstName.value= "Thanh Minh";
+            console.log(car);
+        }
+
+        return { firstName, onChangeSomething};
+    },
+};
+</script>
