64982505f082c4729aff6d44f3c0db880f623bd5
thanhminh <you@example.com>
2022-09-26 10:24:24 +0700

    Add Lesson-12

--- Lesson-12/i-money/src/App.vue
+++ Lesson-12/i-money/src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <component :is="layout">
+    <router-view />
+  </component>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    setup() {
+      return {
+        layout
+      }
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
